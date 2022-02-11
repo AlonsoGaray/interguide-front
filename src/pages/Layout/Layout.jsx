bad4e43947e125eb5aac0b4df20ea68087ec3380
AlonsoGaray <alonsogarayp@gmail.com>
2022-02-10 22:30:27 -0500

    Componente Footer y Layout

--- src/pages/Layout/Layout.jsx
+++ src/pages/Layout/Layout.jsx
@@ -1,14 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
 import './Layout.scss';
 
 const App = () => (
   <>
     <Header />
-    <div className="page-content">
-      <Outlet />
-      {/* <Footer /> */}
-    </div>
+    <Outlet />
+    <Footer />
   </>
 );
 export default App;
